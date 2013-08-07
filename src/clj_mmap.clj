dfd82b9d0d89b26a549ff4554da5c98372975e89
Alan Busby <thebusby@gmail.com>
2013-08-08 00:31:06 +0900

    Added put-bytes, so you can now write to a buffer, removed Counted interface from Mmap, and added .size to return total number of bytes in Mmap

--- src/clj_mmap.clj
+++ src/clj_mmap.clj
@@ -6,13 +6,19 @@
   "The number of bytes a single MappedByteBuffer will store"
   java.lang.Integer/MAX_VALUE)
 
+(definterface ISize
+  (^long size []))
+
 (deftype Mmap [^java.io.FileInputStream fis ^java.nio.channels.FileChannel fc maps]  
+  ISize
+  (size [this] (.size fc))
+
   clojure.lang.Indexed 
   (nth [this i] (get maps i))
   (nth [this i not-found] (get maps i not-found))
 
-  clojure.lang.Counted
-  (count [this] (.size fc))
+  clojure.lang.Seqable
+  (seq [this] (seq maps))
 
   java.io.Closeable
   (close 
@@ -71,3 +77,38 @@
         (.get end-chunk buf read-size (- n read-size))))
 
     buf))
+
+(defn put-bytes
+  "Write n bytes from buf into mmap, at byte position pos.
+   If n isn't provided, the size of the buffer provided is used."
+  ([mmap ^bytes buf pos] (put-bytes buf pos (.size buf)))
+  ([mmap ^bytes buf pos n]
+     (let [get-chunk   #(nth mmap (int (/ % bytes-per-map)))
+           end         (+ pos n)
+           chunk-term  (-> pos
+                           (/ bytes-per-map)
+                           int
+                           inc
+                           (* bytes-per-map))
+           write-size   (- (min end chunk-term) 
+                          pos)
+           start-chunk (get-chunk pos)
+           end-chunk   (get-chunk end)]
+
+       (locking start-chunk 
+         (.position start-chunk (mod pos bytes-per-map))
+         (.put start-chunk buf 0 write-size))
+
+       ;; Handle writes that span MappedByteBuffers
+       (if (not= start-chunk end-chunk)
+         (locking end-chunk 
+           (.position end-chunk 0)
+           (.put end-chunk buf write-size (- n write-size))))
+
+       nil)))
+
+(defn loaded? [mmap]
+  "Returns true if it is likely that the buffer's contents reside in physical memory."
+  (every? (fn [^java.nio.MappedByteBuffer buf] 
+            (.isLoaded buf))  
+          mmap))
