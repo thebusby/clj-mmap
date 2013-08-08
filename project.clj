144db6949e8854421052ee5fbab2274dd7a3fc9e
Alan Busby <thebusby@gmail.com>
2013-08-08 19:05:25 +0900

    Changed from CC0 license to MIT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-mmap "1.1.1"
   :description "A wrapper over java.nio's mmap() implementation to ease use, and enable mmap'ing files larger than 2GB."
   :url "https://github.com/thebusby/clj-mmap"
-  :license {:name "CC0"
-            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
